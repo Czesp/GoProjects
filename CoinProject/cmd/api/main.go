2a5914f99a4bf419a1d86db04e7a98031257c50a
PrajwalBhattarai <prajwalbhattarai1111@gmail.com>
2025-12-11 16:25:05 +0545

    Initial Commit

---
+++ CoinProject/cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"net/http"
+	"fmt"
+
+	"github.com/go-chi/chi"
+	"github.com/Czesp/GoProjects/CoinProject/internal/handlers"
+
+	log "github.com/sirupsen/logrus"
+)
---
+++ CoinProject/go.mod
@@ -0,0 +1,3 @@
+module github.com/Czesp/GoProjects/CoinProject
+
+go 1.25.4
